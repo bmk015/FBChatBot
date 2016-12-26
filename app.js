8fc4bd3c85848bba88f6a627659f6d0662c9fc48
bmk015 <bharathmkgowda@gmail.com>
2016-12-26 19:55:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -835,21 +835,6 @@ function callWelcomeSendAPI(){
 	function(error,
 	response,
 	body){
-		if(!error&&response.statusCode==200){
-			varrecipientId=body.recipient_id;varmessageId=body.message_id;if(messageId){
-				console.log("Successfully sent message with id %s to recipient %s",
-				messageId,
-				recipientId);
-			}else{
-				console.log("Successfully called Send API for recipient %s",
-				recipientId);
-			}
-		}else{
-			console.error("Failed calling Send API",
-			response.statusCode,
-			response.statusMessage,
-			body.error);
-		}
 	});
 }
 
