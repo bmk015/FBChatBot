7e30c2fd4dae8b9ab90a4763017874d8af30b095
bmk015 <bharathmkgowda@gmail.com>
2016-12-26 19:01:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/webhook', function (req, res) {
           console.log("Webhook received unknown messagingEvent: ", messagingEvent);
         }
       });
-    }); */
+    }); 
 
     // Assume all went well.
     //
